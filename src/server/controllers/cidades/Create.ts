f11f91c8b22af6e78703147af43d778c20f7f0cd
Evilazio Neto <evilazioneto943@gmail.com>
2024-06-12 16:05:50 -0300

    add jest

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,26 +1,19 @@
-import { Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import * as yup from 'yup';
 import { validation } from "../../shared/middlewares";
+import { StatusCodes } from "http-status-codes";
 
 interface ICidade {
     nome: string;
-    estado: string
-}
-
-interface IFilter {
-    filter?: string;
 }
 
 export const createValidation = validation((getSchema) => ({
     body: getSchema<ICidade>(yup.object().shape({
-        nome: yup.string().required().min(3),
-        estado: yup.string().required().min(2).max(2).required()
-    })),
-    query: getSchema<IFilter>(yup.object().shape({
-        filter: yup.string().required().min(3),
+        nome: yup.string().required().min(3)
     }))
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-    return res.send('Create!')
+    console.log(req.body)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!')
 }
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,10 @@ router.get('/', (req, res) => {
     return res.json('Olá, DEV!');
 })
 
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll)
 router.post('/cidades', CidadesController.createValidation, CidadesController.create)
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById)
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById)
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById)
 
 export {router}
